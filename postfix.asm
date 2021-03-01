1328cd8a8259f2c800a5b4940f0308ef11f5cf55
5ayam5 <sayam.sethi2@gmail.com>
2021-03-02 00:20:13 +0530

    char checks implemented

--- postfix.asm
+++ postfix.asm
@@ -9,11 +9,34 @@
 # setting and loading initial values along with handling base case
 main:
 	jal		readFile			# reads file
+	move	$s0, $v0			# file descriptor is stored in s0 register
+	lw		$s1, MAX			# load max size
+	li		$s2, 0				# current size of stack
 # loop the reading of file until EOF
 loop:
 	jal		readChar			# reads the next char
 	beq		$v0, 0, printRes	# EOF reached, print the result
-	# @TODO: what is to be done with the char
+	lw		$a0, buffer			# load buffer to register
+	blt		$a0, 49, operate	# not a digit, perform operation
+	blt		$a0, 59, push		# a digit, push to stack
+	j		fileError			# invalid character
+	li		$v0, 1
+	syscall
+
+# identify operator and perform operation
+operate:
+	beq		$a0, 42, product	# operator is *
+	beq		$a0, 43, sum		# operator is +
+	beq		$a0, 45, diff		# operator is -
+	j		fileError
+
+product:
+	j		loop
+
+sum:
+	j		loop
+
+diff:
 	j		loop
 
 # reads the file
@@ -24,7 +47,6 @@ readFile:
 	li		$a2, 0				# file mode: UNIX style (irrelevant since only single line)
 	syscall
 	blt		$v0, 0, fileError	# file could not be read
-	move	$s0, $v0			# file descriptor is stored in s0 register
 	jr		$ra
 
 # prints error message on file not being read and terminates
@@ -37,7 +59,7 @@ fileError:
 readChar:
 	li		$v0, 14				# syscall for file_read
 	move	$a0, $s0			# load the file descriptor always (for safety)
-	la		$a1, buffer			# the buffer location ($t0)
+	la		$a1, buffer			# the buffer location
 	li		$a2, 1				# 1 byte = 1 char to be read
 	syscall
 	beq		$v0, -1, fileError	# error in reading file
@@ -64,13 +86,19 @@ print:
 	syscall
 	jr		$ra
 
+push:
+	j		loop
 
-
+# ####
 # data
 	.data
 buffer:
 	.space	1
+stack:
+	.space	400000				# 4e5 space allcated for the stack
+MAX:
+	.word	400000				# storing the MAX permitted size
 input:
 	.asciiz	"in"
 fileErrorMsg:
-	.asciiz "File not found or could not be opened.\nTerminating..."
+	.asciiz "File not found or error while reading file.\nTerminating..."
--- rough_array.asm
+++ rough_array.asm
@@ -10,33 +10,14 @@ main:
     # index creation increase index by one everytime because char
     addi $t0, $zero, 0
     sw $s0, arr($t0)
-<<<<<<< HEAD
-        addi $t0, $t0, 4
-    sw $s1, arr($t0)
-        addi $t0, $t0, 4
-=======
     addi $t0, $t0, 4
     sw $s1, arr($t0)
     addi $t0, $t0, 4
->>>>>>> d286eaec0077fc5693795d9626120605b3eb335d
     sw $s2, arr($t0)
 
 
     addi $t0, $zero, 0
 
-<<<<<<< HEAD
-    ############################################
-    #while loop
-    ############################################
-    while:
-        beq $t0, arr.size, exit
-
-        lw $t6, arr($t0)
-
-#print char and newline
-        li $v0, 1
-        move $a0, $t6
-=======
     # ###########################################
     # while loop
     # ###########################################
@@ -48,7 +29,6 @@ main:
 # print char and newline
         li $v0, 1
         move $a0, $t5
->>>>>>> d286eaec0077fc5693795d9626120605b3eb335d
         syscall
 
         li $v0, 4
@@ -56,30 +36,16 @@ main:
         syscall
 
 
-<<<<<<< HEAD
-        addi $t0, $t0,1
-        j while
-        
-    exit:
-        li $v0,10
-        syscall
-=======
         addi $t0, $t0,4
         j while
         
 exit:
     li $v0,10
     syscall
->>>>>>> d286eaec0077fc5693795d9626120605b3eb335d
 
 
 
 
 .data
-<<<<<<< HEAD
-    arr: .space 4*n #size of arr needed (4 bytes per int and 1 byte per char)
-    n: .word 3
-=======
-    arr: .space 3 # size of arr needed (4 bytes per int and 1 byte per char)
->>>>>>> d286eaec0077fc5693795d9626120605b3eb335d
+    arr: .space 12 # size of arr needed (4 bytes per int and 1 byte per char)
     newline: .asciiz "\n"
